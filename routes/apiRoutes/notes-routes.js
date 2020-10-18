2234810c04fc68e4f0502be680b1fdc659f07f8a
ASC8 <asamcent@gmail.com>
2020-10-18 14:32:17 -0700

    functions edited

--- routes/apiRoutes/notes-routes.js
+++ routes/apiRoutes/notes-routes.js
@@ -1,4 +1,32 @@
-const { createNote } = require('../../db/notes');
-const { notes } = require('../../db/db.json');
-const router = require('express').Router();
+const notesCalls = require("../../controllers/noteController");
+const router = require("express").Router();
 
+// GET /api/notes returns all notes from the file
+router.get("/notes", (req, res) => {
+
+    notesCalls
+        .getNotes()
+        .then((notes) => 
+            res.json(notes))
+        .catch((err) => 
+            res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+    notesCalls
+        .addNote(req.body)
+        .then((note) => 
+            res.json(note))
+        .catch((err) => 
+            res.status(500).json(err));
+});
+
+// Delete specific note with the assigned UUID
+router.delete("/notes/:id", (req, res) => {
+    notesCalls
+    .deleteNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const path = require('path');
 
 // /notes will be notes.html file
-router.get("notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
 // Other files shall all respond with index.html
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports =router;
